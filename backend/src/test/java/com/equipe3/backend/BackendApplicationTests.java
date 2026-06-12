1a0d1eaa8d694b7440033afa9e505584c139d575
Michael-Anderlin <michael@exthand.com>
2026-06-12 11:32:40 +0200

    Initialize Spring Boot project with Maven wrapper, default configuration, and a simple application skeleton.

---
+++ backend/src/test/java/com/equipe3/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equipe3.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
